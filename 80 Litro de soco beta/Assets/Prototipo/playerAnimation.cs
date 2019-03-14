bd6ab618f44f6e9c516b900633c2802987434c0a
Jovanny Loesch Berté <jovanny.berte@gmail.com>
2019-03-14 16:09:32 -0300

    Base de movimentação semi-completa

---
+++ 80 Litro de soco beta/Assets/Prototipo/playerAnimation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerAnimation : MonoBehaviour{
+
+    protected Animator animator;
+
+    public bool currentlyPunching;
+    
+    // Start is called before the first frame update
+    void Start(){
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.JoystickButton1)){
+            currentlyPunching = true;
+        }
+
+        animator.SetBool("soco", currentlyPunching);
+    }
+
+    void StopPunching(){
+        currentlyPunching = false;
+    }
+}
