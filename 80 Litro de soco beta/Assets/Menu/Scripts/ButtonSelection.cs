20d1ad854acca11e6a5bcb6b1b854b11bbef13dd
Gabriel Fardoski <gabriel.fardoski@gmail.com>
2019-05-12 16:56:24 -0300

    Menu
    Criação do Menu básico

---
+++ 80 Litro de soco beta/Assets/Menu/Scripts/ButtonSelection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonSelection : MonoBehaviour
+{
+    private bool selected = false;
+    void Update()
+    {
+        //        if (EventSystem.current.currentSelectedGameObject == true)
+        //        {
+        //            Debug.Log("selecionado");
+        //        }
+        //
+        //        else
+        //        {
+        //            Debug.Log("deselecionado");
+        //        }
+    }
+}
---
+++ 80 Litro de soco beta/Assets/Menu/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+    public void ExtraGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+    
+    public void ExitGame()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+    
+    /*
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }*/
+}
